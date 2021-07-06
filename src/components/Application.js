181284ff3ee0fe3c9c4e2c1d15cd6394f8bb190f
Adam Underhill <adameunderhill@gmail.com>
2021-07-06 15:00:08 -0700

    fixing error with use state call in app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
 
-
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList"
@@ -26,6 +25,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday")
   return (
     <main className="layout">
       <section className="sidebar">
@@ -38,8 +38,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
 <DayList
   days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
+  day={day}
+  setDay={setDay}
 />
 </nav>
 <img
