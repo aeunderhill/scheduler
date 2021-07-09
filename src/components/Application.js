a4828fc332547fba7d9558fff42188b3ee2c99ad
Adam Underhill <adameunderhill@gmail.com>
2021-07-08 18:35:15 -0700

    selecting each appointment by day from state in application

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,46 +6,16 @@ import DayList from "./DayList"
 import "components/DayListItem.js"
 import Appointment from "components/Appointment"
 import axios from "axios"
+import { getAppointmentsForDay } from "helpers/selectors"
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Michael Jordan",
-      interviewer: {
-        id: 2,
-        name: "Phil Jackson",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "6pm"
-  }
-];
 
-const appt = appointments.map((appointment) => {
+
+const appointmentObjs = getAppointmentsForDay(state, state.day)
+
+const appt = appointmentObjs.map((appointmentObj) => {
   return (
-    <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+    <Appointment key={appointmentObj.id} id={appointmentObj.id} time={appointmentObj.time} interview={appointmentObj.interview} />
   )
 })
 
@@ -61,15 +31,16 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({...prev, days}));
+  //const setDays = days => setState(prev => ({...prev, days}));
 
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then(res => {
-        setDays(res.data)
-      })
-      .catch(err => console.log(err))
+    Promise.all([
+      Promise.resolve(axios.get('http://localhost:8001/api/days')),
+      Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    })
   }, [])
 
 
